5f1f96529259f8922e4c23c6408965d8f69e9441
LittleSheep <littlesheep.code@hotmail.com>
2024-12-19 22:56:07 +0800

    :rocket: Ready to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,12 @@ import mdx from '@astrojs/mdx'
 
 import sitemap from '@astrojs/sitemap'
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://solsynth.dev',
+
   integrations: [
     tailwind(),
     icon(),
@@ -21,7 +24,9 @@ export default defineConfig({
       i18n: { defaultLocale: 'en', locales: { en: 'en-US', 'zh-cn': 'zh-CN' } },
     }),
   ],
+
   prefetch: true,
+
   i18n: {
     locales: ['en', 'zh-cn'],
     defaultLocale: 'en',
@@ -30,4 +35,6 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
+
+  adapter: vercel(),
 })
