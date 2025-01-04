423b3dac05b31586b649243700a4078267d6ce2c
LittleSheep <littlesheep.code@hotmail.com>
2025-01-04 18:40:16 +0800

    :chart_with_upwards_trend: Add vercel speed insights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ import {
   DocumentHeadTagsProps,
   documentGetInitialProps,
 } from '@mui/material-nextjs/v15-pagesRouter'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Html, Head, Main, NextScript, DocumentContext, DocumentProps } from 'next/document'
 
 export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
@@ -16,6 +17,7 @@ export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
         <body className="antialiased">
           <Main />
           <NextScript />
+          <SpeedInsights />
         </body>
       </Html>
     </AppCacheProvider>
