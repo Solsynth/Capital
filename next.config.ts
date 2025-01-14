45dd0ebcb95c85ee3aed71df7a1e94ea7d958d7d
LittleSheep <littlesheep.code@hotmail.com>
2025-01-14 18:39:25 +0800

    :bricks: Using RoadSign instead of Vercel to deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'standalone',
   generateBuildId: async () => {
     return process.env.GIT_HASH ?? 'development'
   },
